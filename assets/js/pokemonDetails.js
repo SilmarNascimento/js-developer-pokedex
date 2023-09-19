e7612dc1f57f03f0a41e5956235e4a7cfb489958
SilmarNascimento <silmarfnascimento@gmail.com>
2023-09-19 09:26:07 -0300

    feat: implementa a lógica para ir para a página de detalhes do pokemon e voltar para a lista de pokemon

--- assets/js/pokemonDetails.js
+++ assets/js/pokemonDetails.js
@@ -1,19 +1,27 @@
 let details = false;
+const [contentList] = document.getElementsByClassName("content");
 
 const createPokemonDetailsCard = (pokeObj) => {
-  console.log(pokeObj);
-  const [contentList] = document.getElementsByClassName("content");
-  console.log(contentList);
   contentList.style.display = 'none';
   const cardContainer = document.createElement('div');
   cardContainer.innerHTML = createCard(pokeObj);
-  document.body.appendChild(cardContainer)
+  document.body.appendChild(cardContainer);
+  const backpageBtn = document.getElementById('back-to-list');
+  backpageBtn.addEventListener('click', backToPageList)
+}
+
+const backToPageList = (cardContainer) => {
+  contentList.style.display = '';
+  const cardElement = document.getElementById('card-details')
+  cardElement.parentNode.removeChild(cardElement);
+
+    
 }
 
 const createCard = (pokeObj) => {
     const imageURL = pokeObj.sprites.other.dream_world.front_default;
     return `
-    <section>
+    <section id="card-details">
         <div class="btn-header">
             <button id="back-to-list" type="button">
                 <--
@@ -24,16 +32,16 @@ const createCard = (pokeObj) => {
         </div>
         <div>
             <h1>${pokeObj.name}</h1>
-            <span>${pokeObj.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}</span>
+            <span>${pokeObj.types.map(({type}) => `<li class="type ${type.name}">${type.name}</li>`).join('')}</span>
             <span>${pokeObj.id}</span>
             <img src="${imageURL}" alt="imagem do pokemon">
         </div>
         <section>
             <h2>Status</h2>
             <ul>
-                ${pokeObj.stats.map((status) => {
-                    const statusName = Object.keys(status.stat)[0];
-                    const statusValue = Object.values(status.stat)[0];
+                ${pokeObj.stats.map(({stat, base_stat}) => {
+                    const statusName = stat.name;
+                    const statusValue = base_stat;
                     return `<li>${statusName}: ${statusValue}</li>`;
                 }).join('')}
             </ul>
