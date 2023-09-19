97cb87fc80ba3c7c7144e00984d80eb0476f25c9
SilmarNascimento <silmarfnascimento@gmail.com>
2023-09-19 08:53:33 -0300

    feat: implementa o card de detalhes e mostra na página

--- assets/js/pokemonDetails.js
+++ assets/js/pokemonDetails.js
@@ -2,25 +2,46 @@ let details = false;
 
 const createPokemonDetailsCard = (pokeObj) => {
   console.log(pokeObj);
-  const contentList = document.getElementsByClassName("content");
+  const [contentList] = document.getElementsByClassName("content");
   console.log(contentList);
-  contentList[0].style.display = 'none';
+  contentList.style.display = 'none';
+  const cardContainer = document.createElement('div');
+  cardContainer.innerHTML = createCard(pokeObj);
+  document.body.appendChild(cardContainer)
 }
 
 const createCard = (pokeObj) => {
+    const imageURL = pokeObj.sprites.other.dream_world.front_default;
     return `
     <section>
-    <div>
-        <h1>${pokeObj.name}</h1>
-        <span>${pokeObj.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}</span>
-        <span>${pokeObj.id}</span>
-        <img src="" alt="imagem do pokemon">
-    </div>
-    <section>
-        <ul>
-            ${pokeObj.abilities.map((ability) => `<li>${ability.name}</li>`).join('')}
-        </ul>
-    </section>
+        <div class="btn-header">
+            <button id="back-to-list" type="button">
+                <--
+            </button>
+            <button id="favorit-list" type="button">
+                <3
+            </button>   
+        </div>
+        <div>
+            <h1>${pokeObj.name}</h1>
+            <span>${pokeObj.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}</span>
+            <span>${pokeObj.id}</span>
+            <img src="${imageURL}" alt="imagem do pokemon">
+        </div>
+        <section>
+            <h2>Status</h2>
+            <ul>
+                ${pokeObj.stats.map((status) => {
+                    const statusName = Object.keys(status.stat)[0];
+                    const statusValue = Object.values(status.stat)[0];
+                    return `<li>${statusName}: ${statusValue}</li>`;
+                }).join('')}
+            </ul>
+            <h2>Abilities</h2>
+                <ul>
+                    ${pokeObj.abilities.map((ability) => `<li>${ability.name}</li>`).join('')}
+                </ul>
+        </section>
     </section>
     `
 }
